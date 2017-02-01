35b6c035e1845bc452ff3c80e777b5b769ba46a4
aacevedoday <aacevedo@dayscriopt.com>
2017-01-31 20:43:33 -0800

    push config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '9a926507'
+  }
+};
 @NgModule({
   declarations: [
     MyApp,
